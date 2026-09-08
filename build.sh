72a12bde30987f0d0ea5bf012764c44b47c47346
Devajyoti Ghosh <devajyoti159357@gmail.com>
2026-09-08 23:00:49 +0530

    Run seed_data in build.sh

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 python seed_data.py
 python -c "import django, os; os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings'); django.setup(); from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'AdminPass123')"
+python seed_data.py
+python -c "import django, os; os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings'); django.setup(); from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'AdminPass123')"
